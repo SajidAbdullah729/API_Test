fa9d4578531d95f23449fb91470b546b604157fe
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2025-03-29 07:38:12 +0600

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -1,7 +1,7 @@
 const users = [
     { id: 1, name: "Alice", email: "alice@example.com" },
     { id: 2, name: "Bob", email: "bob@example.com" },
-    { id: 3, name: "Betty", email: "betty@example.com" },
+    { id: 3, name: "Betty", email: "betty@example.com" }
 ];
 
 module.exports = users;
